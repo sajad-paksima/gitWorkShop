5a6cda6559aa044d058f91a34622943ecf179861
mobotop <paksima.80@gmail.com>
2020-03-15 18:51:07 +0330

    add getting input from user

--- untitled1/src/Main.java
+++ untitled1/src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
         System.out.println("please");
+
     }
 }
